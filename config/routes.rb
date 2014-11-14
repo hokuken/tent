3ce49e8f43234d3cd15edc6ebdc2c68f7e3d6960
Shun Hotta <stars.big2men@gmail.com>
2014-11-14 18:17:03 +0900

    Define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,19 @@
 Tent::Engine.routes.draw do
+
+  namespace :admin, path: '/:site_name/tent' do
+    get '/' => 'tent#dashboard'
+    get 'pitch' => 'tent#configure'
+    get 'newpage' => 'pages#new'
+    resources :pages, param: :page_path,
+              only: [:index, :update, :create, :destroy],
+              constraints: { page_path: /.+/ } do
+      member do
+        get '/', to: 'pages#edit', as: :edit
+      end
+    end
+    resources :themes, only: [:index, :show, :destroy]
+  end
+
+  get '/:site_name/:page_path', to: 'pages#show',
+      constraints: { site_name: /[^\/]+/, page_path: /.*/ }
 end
