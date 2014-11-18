c3b133bb79865575b95375a4864f75da5a633242
Shun Hotta <stars.big2men@gmail.com>
2014-11-18 11:52:05 +0900

    Fix migrations
    * Add comments for included to other App
    * Change index of pages table

--- db/migrate/20141114081425_create_tent_pages.rb
+++ db/migrate/20141114081425_create_tent_pages.rb
@@ -1,22 +1,42 @@
 class CreateTentPages < ActiveRecord::Migration
   def change
     create_table :tent_pages do |t|
+      t.integer :site_id,        null: false
+      # Page path: Default page is 'index'
       t.string  :path,           null: false
+
+      # Meta data
       t.string  :title
       t.text    :description
       t.text    :keywords
+
+      # Custom Javascript and Head section
       t.text    :javascript
       t.text    :head
+
+      # Body data and body parser
       t.text    :body,           null: false, default: ''
+      t.string  :parser,         null: false, default: 'markdown'
+
+      # Template file name
       t.string  :template_name
+
+      # Parent page id
+      t.integer :parent_id
+
+      t.integer :nav_page_id
       t.integer :menu_page_id
       t.integer :footer_page_id
+
+      # Published to guests
       t.integer :published
+
+      # Show on sitemap and Be crawlable
       t.integer :indexed
 
       t.timestamps
 
-      t.index :path, unique: true
+      t.index [:site_id, :path], unique: true
     end
   end
 end
