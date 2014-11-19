249bbfdad2383f24ac489236a7071e1f44bc78de
masamori <r.masamori@gmail.com>
2014-11-19 18:39:37 +0900

    Add index create new method in sites controller

--- app/controllers/tent/sites_controller.rb
+++ app/controllers/tent/sites_controller.rb
@@ -3,6 +3,9 @@ require_dependency "tent/application_controller"
 module Tent
   class SitesController < ApplicationController
     def index
+      @sites = Site.all
+      @site = Site.new
+      @host_url = main_app.root_url
     end
 
     def new
