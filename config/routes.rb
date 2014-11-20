48446db23737340c5b33c94db54a8b964a7a33bf
Shun Hotta <stars.big2men@gmail.com>
2014-11-20 14:38:15 +0900

    Update route for pages#show.
    * Name as site_page
    * Allow empty page path

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Tent::Engine.routes.draw do
     resources :themes, only: [:index, :show, :destroy]
   end
 
-  get '/:site_path/:page_path', to: 'pages#show',
-      constraints: { site_name: /[^\/]+/, page_path: /.*/ }
+  get '/:site_path(/:page_path)', to: 'pages#show',
+      constraints: { site_name: /[^\/]+/, page_path: /.*/ },
+      as: :site_page
 end
