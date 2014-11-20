788be00e513c0b42e7614efe51ed2c9cedd21db4
Shun Hotta <stars.big2men@gmail.com>
2014-11-20 19:34:00 +0900

    Load files in lib/tent

--- lib/tent/engine.rb
+++ lib/tent/engine.rb
@@ -8,6 +8,8 @@ module Tent
   class Engine < ::Rails::Engine
     isolate_namespace Tent
 
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
     config.generators do |g|
       g.test_framework :rspec
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
