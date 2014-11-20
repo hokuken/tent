fe0db4cf8309a985df7bccbab34cf060b511f5ea
Shun Hotta <stars.big2men@gmail.com>
2014-11-20 16:40:19 +0900

    Complete Site tests

--- app/models/tent/site.rb
+++ app/models/tent/site.rb
@@ -2,13 +2,28 @@ module Tent
   class Site < ActiveRecord::Base
     mount_uploader :logo_url, LogoUploader
 
-    before_validation { path.downcase! }
+    validates :title, :path, presence: true
+    validates :path, length: { maximum: 50 }
+    validates :path, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々]|[a-z0-9_-])+\z/ }
+
+    before_validation :replace_control_characters
+
+    before_validation do
+      path.downcase!
+      path.strip!
+    end
+
+
     after_create :insert_index_page
 
     private
 
     def insert_index_page
       Page.create site_id: id, path: :index
+    end
+
+    def replace_control_characters
+      path.gsub! /[[:cntrl:]]/, " "
     end
   end
 end
