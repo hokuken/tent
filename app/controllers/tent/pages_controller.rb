ad68a17fb9e4d9f67accaa39e6097189892afd24
Shun Hotta <stars.big2men@gmail.com>
2014-11-20 15:20:13 +0900

    Fix param name

--- app/controllers/tent/pages_controller.rb
+++ app/controllers/tent/pages_controller.rb
@@ -3,8 +3,8 @@ require_dependency "tent/application_controller"
 module Tent
   class PagesController < ApplicationController
     def show
-      site_name = params[:site_name]
-      page_path = params[:page_path]
+      site_name = params.fetch :site_path
+      page_path = params.fetch :page_path, :index
       raise "#{site_name} : #{page_path}"
     end
 
