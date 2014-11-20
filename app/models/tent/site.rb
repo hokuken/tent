944b00e8018329ff388934bc6aea9a0bf3fe4f51
Shun Hotta <stars.big2men@gmail.com>
2014-11-20 15:19:30 +0900

    Complete Site tests

--- app/models/tent/site.rb
+++ app/models/tent/site.rb
@@ -2,5 +2,13 @@ module Tent
   class Site < ActiveRecord::Base
     mount_uploader :logo_url, LogoUploader
 
+    before_validation { path.downcase! }
+    after_create :insert_index_page
+
+    private
+
+    def insert_index_page
+      Page.create site_id: id, path: :index
+    end
   end
 end
